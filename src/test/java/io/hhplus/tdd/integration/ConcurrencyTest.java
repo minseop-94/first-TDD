2d4e3219af841ee59fb82defd63d4c2700327454
b_loso <jaesme2530@gmail.com>
2024-06-21 20:20:44 +0900

    [ADD] 동시성 테스트용 Integration Test용 Test class추가, 주석 추가.

---
+++ src/test/java/io/hhplus/tdd/integration/ConcurrencyTest.java
@@ -0,0 +1,9 @@
+package io.hhplus.tdd.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+// Integration Test -> 의존성이 필요
+@SpringBootTest
+public class ConcurrencyTest {
+
+}
