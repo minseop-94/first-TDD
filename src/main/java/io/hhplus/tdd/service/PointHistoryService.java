410cdccc1d2b5a6e4f59a1b0e74387a9b704676c
b_loso <jaesme2530@gmail.com>
2024-06-21 20:36:47 +0900

    [FIX] 통합테스트 Spring 의존성 주입 실패 에러 해결

--- src/main/java/io/hhplus/tdd/service/PointHistoryService.java
+++ src/main/java/io/hhplus/tdd/service/PointHistoryService.java
@@ -2,9 +2,11 @@ package io.hhplus.tdd.service;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.point.PointHistory;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PointHistoryService {
   private PointHistoryTable pointHistoryTable;
 
